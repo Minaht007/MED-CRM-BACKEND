e116b58d827e15c2715911fb2de1660d7eddbd44
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-06-12 21:15:24 +0300

    prepear to routers

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
