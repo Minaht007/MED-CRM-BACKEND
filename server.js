5d13b446240bdb42a5ad29031fe5d39ea09cb3c5
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-06-12 14:42:49 +0300

    1

--- server.js
+++ server.js
@@ -14,14 +14,3 @@ mongoose
     console.log(error.messages);
     process.exit(1);
   });
-
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     app.listen(3000);
-//     console.log("Database connection successful");
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
