3ec91dcd3f980d1827624413180c923c5f755a85
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-10-17 11:41:28 +0300

    shemas

--- models/clientSchema.js
+++ models/clientSchema.js
@@ -3,9 +3,18 @@ const { Schema, model } = require("mongoose");
 const clientSchema = new Schema(
   {
     name: { type: String, required: true },
-    phone: { type: String, required: true },
+    phone: { type: String },
     email: { type: String, required: true },
     location: { type: String },
+    sex: {type: String},
+    weight: {type: Number},
+    height: {type: Number},
+    blood: {type: String},
+    rhesus: {type: String},
+    allergies: {type: String},
+    vaccinec: {type: String},
+    genetics: {type: String},
+    operations: {type: String}
   },
   { versionKey: false, timestamps: true }
 );
--- models/doctorSchema.js
+++ models/doctorSchema.js
@@ -5,9 +5,21 @@ const doctorSchema = new Schema(
     name: { type: String, required: true },
     secondName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    speciality: { type: String, required: true },
+    phone: { type: String },
+    speciality: { type: String },
+    specialization: {type: String},
+    qualification: {type: String},
+    scientific: {type: String},
+    experiens: {type: String},
+    price: {type: String},
     password: { type: String, required: true },
+    education: { type: String, required: true },
+    trainings: { type: String, required: true },
+    association: { type: String, required: true },
+    procedures: { type: String, required: true },
+    diplomes: { type: String, required: true },
+    desases: { type: String, required: true },
+    departure: {type: true}
   },
   { versionKey: false, timestamps: true }
 );
