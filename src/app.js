09638705a53f438729a655453e5cb7400ee67fbe
Andrey <108063187+Minaht007@users.noreply.github.com>
2025-10-12 18:13:12 +0300

    1234567

--- src/app.js
+++ src/app.js
@@ -24,16 +24,16 @@ const URL = "https://elexir-five.vercel.app/"
 // const URL = "http://localhost:3000";
 // const URL = "https://docworldbase.netlify.app";
 
-// const corsOptions = {
-// 	origin: [URL],
-// 	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-// 	allowedHeaders: ["Content-Type", "Authorization"],
-// 	credentials: true,
-// };
-origin: [
-    "https://your-app.vercel.app", 
-    "http://localhost:3000",       
-  ],
+const corsOptions = {
+	origin: [
+		"https://your-app.vercel.app", 
+		"http://localhost:3000",       
+	  ],
+	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+	credentials: true,
+};
+
 
 
 app.use(morgan("combined"));
